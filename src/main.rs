55d499449be94e2229a88c98e89513b1f6c2426b
tadamak <tadamak@gmail.com>
2021-11-21 14:46:31 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use z2p::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
